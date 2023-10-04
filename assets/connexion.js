366ccdea3b658c2316af96ecd4874008c4f83273
BINARD Théo <theo.binard@gruau.com>
2023-10-04 13:59:45 +0200

    Initial commit (suite 2)

--- assets/connexion.js
+++ assets/connexion.js
@@ -1,13 +1,30 @@
 document.getElementById('inputEmploye').addEventListener('input', function () {
-    // api request
+    if(this.value == "") {
+        document.getElementById('informationEmploye').classList.remove('loading', 'loading-dots', 'loading-lg', 'text-gruau-dark-blue')
+        document.getElementById('informationEmploye').innerText = "Information employé";
+        return;
+    }
+    // Afficher "Chargement..."
+    document.getElementById('informationEmploye').innerText = "";
+    document.getElementById('informationEmploye').classList.add('loading', 'loading-dots', 'loading-lg', 'text-gruau-dark-blue')
 
     var input = document.getElementById('inputEmploye');
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'api/get/employe/' + input.value);
-    xhr.send();
-    xhr.onload = function () {
-        var employe = JSON.parse(xhr.responseText);
-        document.getElementById('informationEmploye').innerText = employe.nom + " " + employe.prenom;
-    }
+    var url = 'api/get/employe/' + input.value;
 
-})
+    fetch(url)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Employé non trouvé');
+            }
+        })
+        .then(function (employe) {
+    document.getElementById('informationEmploye').classList.remove('loading', 'loading-dots', 'loading-lg', 'text-gruau-dark-blue')
+    document.getElementById('informationEmploye').innerText = employe.nom + " " + employe.prenom;
+        })
+        .catch(function (error) {
+    document.getElementById('informationEmploye').classList.remove('loading', 'loading-dots', 'loading-lg', 'text-gruau-dark-blue')
+    document.getElementById('informationEmploye').innerText = error.message;
+        });
+});
