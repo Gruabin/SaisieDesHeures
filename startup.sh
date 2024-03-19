6f2bb61a4e607a81e88718090d23f66df0425da5
Brieuc Courapié <Brieuc.Courapie@gruau.com>
2024-03-19 12:35:36 +0000

    Updated startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
+php bin/console cache:clear
 cp /home/site/wwwroot/nginx.conf /etc/nginx/sites-available/default
 cp /home/site/wwwroot/nginx.conf /etc/nginx/sites-enabled/default
 service nginx reload
