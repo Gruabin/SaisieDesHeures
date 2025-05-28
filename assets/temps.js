d00e70e9045fd4fc666004d75113e5105f2e99f6
Gabin Guerin <gabin.guerin@gruau.com>
2025-05-28 16:00:57 +0200

    Fix #8390 -> Fix de problèmes concernant la tache spécifique de fabrication et l'activité de projet

--- assets/temps.js
+++ assets/temps.js
@@ -10,7 +10,7 @@ function verifChampOrdre(evt){
     }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('turbo:load', function () {
     const checkbox = document.getElementById('cbTacheSpe');
     const selectTacheSpe = document.querySelector('[data-tache-spe="true"]');
 
@@ -25,37 +25,5 @@ document.addEventListener('DOMContentLoaded', function () {
         } else {
             console.error('Champ tâche spécifique introuvable !');
         }
-    });
-});
-
-window.addEventListener('load', function () {
-    const input = document.getElementById('ajout_projet_activite');
-    const info = document.getElementById('infoActivite');
-    console.log('AAAAAAAAAAAH', input, info)
-
-    input.addEventListener('input', function () {
-        const id = input.value.trim();
-
-        input.classList.remove('border-green-500', 'border-red-500');
-        info.textContent = '';
-
-        if (id === '') return;
-
-        fetch(`/verifier-activite/${id}`)
-            .then(response => response.json())
-            .then(data => {
-                if (data.trouve) {
-                    input.classList.add('border-green-500');
-                    info.textContent = `Activité : ${data.nom}`;
-                    info.className = 'text-green-600 mx-3 text-sm mt-1';
-                } else {
-                    input.classList.add('border-red-500');
-                    info.textContent = `Aucune activité trouvée pour cet ID`;
-                    info.className = 'text-red-600 mx-3 text-sm mt-1 font-bold';
-                }
-            })
-            .catch(error => {
-                console.error('Erreur lors de la vérification de l\'activité:', error);
-            });
     });
 });
